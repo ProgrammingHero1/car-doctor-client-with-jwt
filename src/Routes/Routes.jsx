794c4b4409c29da25ff36c5fbd49062b943b9bae
Programming Hero <programming.hero1@gmail.com>
2023-05-09 13:28:34 -0500

    added the checkout route form

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
+import Checkout from "../pages/Checkout/Checkout";
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,11 @@ const router = createBrowserRouter([
         {
             path: 'signup', 
             element: <SignUp></SignUp>
+        },
+        {
+          path: 'checkout/:id', 
+          element: <Checkout></Checkout>, 
+          loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
         }
       ]
     },
