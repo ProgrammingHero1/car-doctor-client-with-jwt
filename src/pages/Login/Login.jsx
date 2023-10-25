38707f0238bfc24f8d962808c07dfce2fde128e6
Programming Hero <programming.hero1@gmail.com>
2023-10-25 08:23:44 -0500

    get token using axios

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
 import { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import axios from 'axios';
 
 const Login = () => {
 
@@ -15,12 +16,19 @@ const Login = () => {
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(name, email, password)
+
         signIn(email, password)
             .then(result => {
-                const user = result.user;
-                console.log(user);
-                navigate(location?.state ? location?.state : '/' )
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                const user ={ email };
+                // navigate(location?.state ? location?.state : '/')
+                // get access token
+                axios.post('http://localhost:5000/jwt', user)
+                .then(res =>{
+                    console.log(res.data)
+                })
+
             })
             .catch(error => console.log(error));
     }
